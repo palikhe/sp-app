6dfb8c08a2971bde4af5df9ec2196bfebd572dbb
Suman Palikhe <spaikhe@woolworths.com.au>
2024-11-15 13:41:13 +1100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import ".//css/App.css";
+import ".//css/style.css";
+import ".//css/animate.css";
+import ".//css/aos.css";
+import ".//css/default.css";
+import ".//css/jquery.fancybox.min.css";
+import ".//css/leaflet.css";
+import ".//css/meanmenu.css";
+import ".//css/responsive.css";
+import ".//css/slick.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
